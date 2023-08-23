65c1ad547f8f02c83a13b86e255b3975afe375a9
Anton Ozerov <lamerork@gmail.com>
2023-08-23 12:09:38 +0300

    Добавил обработку ошибок

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from time import sleep
 from bs4 import BeautifulSoup
 import requests
 from pathlib import Path
@@ -24,9 +25,7 @@ def parse_book_page(response, book_id):
     comments = [comment_tag.text for comment_tag in comment_tags]
     genres_tags = soup.select('span.d_book a')
     genres = [genres_tag.text for genres_tag in genres_tags]
-
-    image_url = urljoin('https://tululu.org', soup.select_one('.bookimage img')['src'])
-
+    image_url = urljoin(response.url, soup.select_one('.bookimage img')['src'])
 
     book = {
             'book_id': book_id,
@@ -39,32 +38,15 @@ def parse_book_page(response, book_id):
     return book
 
 
-def get_books(start_id, end_id):
-
-    for book_id in range(start_id, end_id + 1):
-        try:
-            response = requests.get(f'https://tululu.org/b{book_id}/')
-            response.raise_for_status()
-            check_for_redirect(response)
-            book = parse_book_page(response, book_id)
-            download_txt(f'https://tululu.org/txt.php?id={book_id}', f'{book_id}. {book["book_name"]}')
-            download_image(book['image_url'])
-
-        except requests.exceptions.HTTPError:
-            pass
-
-
-def download_txt(url, filename, folder='books'):
+def download_txt(url, book_id, filename, folder='books'):
 
     Path(folder).mkdir(parents=True, exist_ok=True)
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        file_path = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
-        with open(file_path, 'wb') as file:
-            file.write(response.content)
-    except requests.exceptions.HTTPError:
-        return
+
+    response = requests.get(url, params={'id': book_id})
+    response.raise_for_status()
+    file_path = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
 
     return file_path
 
@@ -83,12 +65,26 @@ def download_image(image_url, folder='images'):
 def main():
 
     parser = argparse.ArgumentParser(description='Парсер книг')
-    parser.add_argument('--start_id', help='Введите стартовый id', default=1, type=int)
-    parser.add_argument('--end_id', help='Введите конечный id', default=10, type=int)
+    parser.add_argument('--start_id', help='Введите стартовый id', default=10, type=int)
+    parser.add_argument('--end_id', help='Введите конечный id', default=100, type=int)
     arguments = parser.parse_args()
     start_id = arguments.start_id
     end_id = arguments.end_id
-    get_books(start_id, end_id)
+
+    for book_id in range(start_id, end_id + 1):
+        try:
+            response = requests.get(f'https://tululu.org/b{book_id}/')
+            response.raise_for_status()
+            check_for_redirect(response)
+            book = parse_book_page(response, book_id)
+            download_txt('https://tululu.org/txt.php', book_id, f'{book_id}. {book["book_name"]}')
+            download_image(book['image_url'])
+
+        except requests.exceptions.HTTPError:
+            print(f'Книга не найдена: {book_id}')
+        except requests.exceptions.ConnectionError:
+            print('Ожидаем соединение 60 секунд')
+            sleep(60)
 
 
 if __name__ == '__main__':
